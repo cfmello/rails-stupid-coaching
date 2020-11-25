9ecd9e26d540d70ba95fdc03e52f771b0f761ecf
Cristhiano Mello <cristhiano.f.mello@gmail.com>
2020-11-25 15:38:36 -0300

    Milene coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
-  def ask
+  def ask; end
+
+  def answer
+    @question = params[:input]
+    @answer = if @question.start_with?('Milene')
+                'Não termine com "É isso..."'
+              elsif @question.end_with?('?')
+                'Silly question, get dressed and go to work!'
+              elsif @question == 'I am going to work right now!'
+                'Great!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
   end
 end
